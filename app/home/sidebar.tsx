1957f0164eddd619f392d64709445a70630b6536
vet3whale <githubvetri@gmail.com>
2025-05-22 15:24:38 +0800

    feat: signout button in homepage

--- app/home/sidebar.tsx
+++ app/home/sidebar.tsx
@@ -1,6 +1,14 @@
-import { FiHome, FiFileText, FiFolder, FiUsers, FiSettings} from "react-icons/fi";
+"use client"
+import { FiHome, FiFileText, FiFolder, FiUsers, FiSettings, FiLogOut} from "react-icons/fi";
+import { useRouter } from "next/navigation";
 
 const SideBar = () => {
+    const router = useRouter();
+
+    const handleSignOut = () => {
+        localStorage.removeItem("isLoggedIn");
+        router.replace("/login");
+    };
     return (
         <div className="fixed top-0 left-0 h-screen w-50 m-0 flex flex-col
                         bg-gray-800 text-white shadow-lg">
@@ -11,8 +19,17 @@ const SideBar = () => {
                 <SideBarIcon icon ={<FiUsers size = "20" />} text = "Friends" />
             </div>
 
-            <div className="mt-auto">
-                <SideBarIcon icon ={<FiSettings size = "20" />} text = "Settings"/>
+                <div className="mt-auto flex flex-col">
+                    <button
+                        onClick={handleSignOut}
+                        className="sidebar-icon group flex items-center p-4 hover:bg-gray-900 cursor-pointer text-left w-full"
+                    >
+                    <FiLogOut size="20" />
+                    <span className="ml-4 text-sm font-normal text-gray-200 group-hover:text-white">
+                        Sign Out
+                    </span>
+                    </button>
+                    <SideBarIcon icon={<FiSettings size="20" />} text="Settings" />
             </div>
         </div>
     );
