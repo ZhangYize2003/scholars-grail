c20058c47e8eb1668a224d19e748fa4eb2a7d0f2
ZhangYize2003 <zhangyz1601@gmail.com>
2025-05-22 17:09:47 +0800

    fix: make hover styling for sidebar icons consistent

--- app/home/sidebar.tsx
+++ app/home/sidebar.tsx
@@ -13,23 +13,31 @@ const SideBar = () => {
         <div className="fixed top-0 left-0 h-screen w-50 m-0 flex flex-col
                         bg-gray-800 text-white shadow-lg">
             <div className="pt-20 space-y-1">
+                <button className="w-full hover:bg-gray-900 transition-colors text-left" 
+                onClick={handleSignOut}> {/* Placeholder function */}
                 <SideBarIcon icon ={<FiHome size = "20" />} text = "Home" />
+                </button>
+                <button className="w-full hover:bg-gray-900 transition-colors text-left" 
+                onClick={handleSignOut}> {/* Placeholder function */}
                 <SideBarIcon icon ={<FiFileText size = "20" />} text = "Mark Paper" />
+                </button>
+                <button className="w-full hover:bg-gray-900 transition-colors text-left" 
+                onClick={handleSignOut}> {/* Placeholder function */}
                 <SideBarIcon icon ={<FiFolder size = "20" />} text = "Repository" />
+                </button>
+                <button className="w-full hover:bg-gray-900 transition-colors text-left" 
+                onClick={handleSignOut}> {/* Placeholder function */}
                 <SideBarIcon icon ={<FiUsers size = "20" />} text = "Friends" />
+                </button>
             </div>
 
                 <div className="mt-auto flex flex-col">
-                    <button
-                        onClick={handleSignOut}
-                        className="sidebar-icon group flex items-center p-4 hover:bg-gray-900 cursor-pointer text-left w-full"
-                    >
-                    <FiLogOut size="20" />
-                    <span className="ml-4 text-sm font-normal text-gray-200 group-hover:text-white">
-                        Sign Out
-                    </span>
+                    <button onClick={handleSignOut}>
+                    <SideBarIcon icon = {<FiLogOut size="20" />} text = "Sign Out" />
                     </button>
-                    <SideBarIcon icon={<FiSettings size="20" />} text="Settings" />
+                    <button onClick={handleSignOut}> {/* Placeholder function */}
+                    <SideBarIcon icon = {<FiSettings size="20" />} text = "Settings" />
+                    </button>
             </div>
         </div>
     );
