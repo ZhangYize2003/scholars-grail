881ab0c9219aa740e83a73aad4042230a0a3dc86
ZhangYize2003 <zhangyz1601@gmail.com>
2025-05-23 01:21:29 +0800

    fix: remove side bar import as it is part of header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import SideBar from "./home/sidebar";
+import Header from "./home/header";
 
 export default function Home() {
   const router = useRouter();
@@ -14,8 +14,8 @@ export default function Home() {
   }, [router]);
 
   return (
-    <div className="flex">
-      <SideBar />
+    <div className="min-h-screen">
+      <Header />
     </div>
   );
 }
