5e17105bd12258d4ded3406b7fd07cac8e768afc
ZhangYize2003 <zhangyz1601@gmail.com>
2025-06-02 00:50:04 +0800

    fix: update homepage background colour

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
   }, [router]);
 
   return (
-  <div className="min-h-screen">
+  <div className="min-h-screen bg-gray-950">
       <Header />
         <h1 className="pt-20 text-gray-200">
           Welcome{userName ? `, ${userName}!` : ""}
