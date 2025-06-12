d22dbc256925ae7c8d419c91f02cf955ec3d5e9c
ZhangYize2003 <zhangyz1601@gmail.com>
2025-06-12 18:06:56 +0800

    fix: simplify code for sidebar and header

--- app/home/profile-menu.tsx
+++ app/home/profile-menu.tsx
@@ -16,25 +16,24 @@ const ProfileMenu = ({isOpen}: ProfileMenuProps) => {
     };
 
     return (
-        <div className={`fixed top-16 right-3 h-40 w-30 bg-secondary shadow-lg z-50
+        <div className={`fixed top-16 right-2 h-40 w-30 bg-secondary shadow-lg z-50
                         flex flex-col justify-between border-t-1 border-stroke rounded-md
-                        ${isOpen ? 'translate-x-0' : 'opacity-0 pointer-events-none'}`}>
+                        ${isOpen ? "translate-x-0" : "opacity-0 pointer-events-none"}`}>
             <div className="flex flex-col mt-3 text-sm text-main font-normal">
-                <Link href="/profile" className="flex items-center p-3 gap-2 hover:bg-tertiary cursor-pointer">
+                <Link href="/profile" className="profile_menu">
                     <FiUser size="20" /> 
                     Profile
                 </Link>
-                <button onClick={handleSignOut} className="flex items-center p-3 gap-2 hover:bg-tertiary cursor-pointer">
+                <button onClick={handleSignOut} className="profile_menu">
                     <FiLogOut size="20" />
                     Sign out
                 </button>
-                <Link href="/" className="flex items-center p-3 gap-2 hover:bg-tertiary cursor-pointer"> {/* Placeholder href */}
+                <Link href="/" className="profile_menu"> {/* Placeholder href */}
                     <FiSettings size="20" />
                     Settings
                 </Link>
             </div>
-        </div>
-    
+        </div> 
     );
 }
 
--- app/home/sidebar.tsx
+++ app/home/sidebar.tsx
@@ -1,48 +1,38 @@
 import Link from 'next/link';
 import { FiHome, FiFileText, FiFolder, FiUsers } from "react-icons/fi";
-import { useRouter } from "next/navigation";
 
 interface SideBarProps {
     isOpen: boolean;
     onClose: () => void;
 }
 
-const SideBar = ({isOpen}: SideBarProps) => {
-    const router = useRouter();
 
-    const handleSignOut = () => {
-        localStorage.removeItem("isLoggedIn");
-        router.replace("/login");
-    };
+const SideBar = ({isOpen}: SideBarProps) => {
+
     return (
         <div className={`fixed top-16 left-0 h-screen w-45 bg-primary shadow-lg transform transition-transform 
                         duration-300 z-50 flex flex-col justify-between border-t-1 border-stroke
-                        ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-            <div className="pt-3">
+                        ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
+            <div className="flex flex-col pt-4 text-sm text-main font-normal">
                 <Link href="/" className="sidebar_button">
-                <SideBarIcon icon ={<FiHome size = "20" />} text = "Home" />
+                    <FiHome size="20" /> 
+                    Home
                 </Link>
-                <Link href="/" className="sidebar_button"> {/* Placeholder href */}
-                <SideBarIcon icon ={<FiFileText size = "20" />} text = "Grail Session" />
+                <Link href="/" className="sidebar_button">
+                    <FiFileText size="20" /> 
+                    Grail Session
                 </Link>
-                <Link href="/repository" className="sidebar_button"> {/* Placeholder href */}
-                <SideBarIcon icon ={<FiFolder size = "20" />} text = "Repository" />
+                <Link href="/repository" className="sidebar_button">
+                    <FiFolder size="20" /> 
+                    Repository
                 </Link>
-                <Link href="/" className="sidebar_button"> {/* Placeholder href */}
-                <SideBarIcon icon ={<FiUsers size = "20" />} text = "Friends" />
+                <Link href="/" className="sidebar_button">
+                    <FiUsers size="20" /> 
+                    Friends
                 </Link>
             </div>
         </div>
     );
 }
-
-const SideBarIcon = ({ icon, text }: { icon: React.ReactNode; text: string}) => (
-    <div className="sidebar-icon group flex items-center p-4 ml-2 hover:bg-secondary cursor-pointer rounded-md">
-        {icon}
-        <span className="ml-4 text-sm font-normal text-gray-200 group-hover:text-white">
-        {text}
-        </span>
-    </div>
-);
 
 export default SideBar;
