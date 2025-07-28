bb76de8a0f896d8cc07fa42f5da7b2e0fc2f7927
ZhangYize2003 <zhangyz1601@gmail.com>
2025-07-28 11:21:07 +0800

    fix: improve profile page UI

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -51,37 +51,37 @@ export default function ProfilePage() {
   }, [router]);
 
   return (
-    <div className="min-h-screen flex items-center-safe justify-center">
+    <div className="flex items-center-safe justify-center min-h-screen text-main">
       <Header />
-      <div className="bg-gray-800 overflow-hidden shadow rounded-lg border border-gray-700 mt-8 max-w-2xl mx-auto">
+      <div className="bg-secondary overflow-hidden shadow rounded-lg border border-tertiary mt-8 min-w-xl max-w-2xl">
         <div className="px-4 py-5 sm:px-6">
-          <h3 className="text-lg leading-6 font-medium text-white">
+          <h3 className="text-lg leading-6 font-bold">
             User Profile
           </h3>
         </div>
-        <div className="border-t border-gray-700 px-4 py-5 sm:p-0">
-          <dl className="sm:divide-y sm:divide-gray-700">
+        <div className="border-t border-tertiary px-4 py-5 sm:p-0">
+          <dl className="sm:divide-y sm:divide-tertiary">
             <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-300">
-                Username
+              <dt className="text-sm font-medium">
+                Username:
               </dt>
-              <dd className="mt-1 text-sm text-white sm:mt-0 sm:col-span-2">
+              <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">
                 {userName || "N/A"}
               </dd>
             </div>
             <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-300">
-                Email address
+              <dt className="text-sm font-medium">
+                Email Address:
               </dt>
-              <dd className="mt-1 text-sm text-white sm:mt-0 sm:col-span-2">
+              <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">
                 {email || "N/A"}
               </dd>
             </div>
             <div className ="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-300">
-                    Account Created
+                <dt className="text-sm font-medium">
+                    Account Created:
                 </dt>
-                <dd className="mt-1 text-sm text-white sm:mt-0 sm:col-span-2">
+                <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">
                     {accountCreated || "N/A"}
                 </dd>
             </div>
