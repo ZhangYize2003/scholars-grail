0c88dddb17445856e39278585815d336f5e1e08d
ZhangYize2003 <zhangyz1601@gmail.com>
2025-07-29 17:18:30 +0800

    fix: remove callBack as it is no longer used

--- app/components/GrailSession.tsx
+++ app/components/GrailSession.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState} from "react";
 import Header from "../home/header";
 import UploadWorking from "./UploadWorking";
 import RevisionModal from "./RevisionModal";
